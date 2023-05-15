f66f3df03eb4a3052374988a46186b7f4a3b398a
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-05-15 14:05:35 +0800

    Prompt the user on channel and volume for both tv1 and tv2

--- television.py
+++ television.py
@@ -55,8 +55,22 @@ class TV:
         if self.on and self.volumeLevel > 1:
             self.volumeLevel -= 1
 
-# adding 2 tv instances
+# Adding 2 tv instances
 class TestTV:
     def main(self):
         tv1 = TV()
         tv2 = TV()
+        
+        # Prompt the user on channel and volume for both tv1 and tv2
+        tv1.turnOn()
+        channel1 = int(input("\033[32mEnter the channel for tv1 (1-120):\033[0m"))
+        while not (1 <= channel1 <= 120):
+            print("033[31mInvalid channel! Please enter a number between 1 and 120.\033[0m")
+            channel1 = int(input("Enter the channel for tv1 (1-120): "))
+        tv1.setChannel(channel1)
+
+        volume1 = int(input("\033[32mEnter the volume level for tv1 (1-7):\033[0m"))
+        while not (1 <= volume1 <= 7):
+            print("033[31mInvalid volume level! Please enter a number between 1 and 7.\033[0m")
+            volume1 = int(input("Enter the volume level for tv1 (1-7): "))
+        tv1.setVolume(volume1)
