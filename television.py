758dd3bc053d51ce6c42ce1a96a232ea7d9aad4f
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-05-15 14:02:38 +0800

    adding 2 instances

--- television.py
+++ television.py
@@ -55,45 +55,8 @@ class TV:
         if self.on and self.volumeLevel > 1:
             self.volumeLevel -= 1
 
-# Define the main method for running the TV program
-def main():
-    # Use pyfiglet to print "TV Program" in ASCII art
-    result = pyfiglet.figlet_format("TV Program", font="slant")
-    print(result)
-
-    # Create a TV instance
-    tv = TV()
-
-# Prompt the user to turn the TV on or off
-    while True:
-        choice = input("Enter 'on' to turn the TV on or 'off' to turn it off: ")
-        if choice.lower() == 'on':
-            tv.turnOn()
-            break
-        elif choice.lower() == 'off':
-            tv.turnOff()
-            break
-        else:
-            print("Invalid choice. Please enter 'on' or 'off'.")
-
-    # Prompt the user to change the channel or adjust the volume level
-    while tv.on:
-        choice = input("Enter 'c' to change the channel, 'v' to adjust the volume level, or 'q' to quit: ")
-        if choice.lower() == 'c':
-            channel = int(input("Enter the channel number (1-120): "))
-            tv.setChannel(channel)
-            print(f"Current channel is {tv.getChannel()}")
-        elif choice.lower() == 'v':
-            volumeLevel = int(input("Enter the volume level (1-7): "))
-            tv.setVolume(volumeLevel)
-            print(f"Current volume level is {tv.getVolume()}")
-        elif choice.lower() == 'q':
-            tv.turnOff()
-            break
-        else:
-            print("Invalid choice. Please enter 'c', 'v', or 'q'.")
-    
-# Call the main method to run the TV program
-if __name__ == '__main__':
-    main()
-
+# adding 2 tv instances
+class TestTV:
+    def main(self):
+        tv1 = TV()
+        tv2 = TV()
