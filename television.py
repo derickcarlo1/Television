6625a0629dc1a0625382e02e560e0d8953ba4a3f
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-05-15 09:55:51 +0800

    plotting pseudocodes based on figure 7.7

---
+++ television.py
@@ -0,0 +1,42 @@
+# Import the pyfiglet module for ASCII art
+
+# Define the TV class
+
+# Initialize the TV with default values
+
+# Turn the TV on
+
+# Turn the TV off
+
+# Get the current channel
+
+# Set the current channel
+
+# Get the current volume level
+
+# Set the current volume level
+
+# Increase the channel by one
+
+# Decrease the channel by one
+
+# Increase the volume level by one
+
+# Decrease the volume level by one
+
+# Define the TestTV class
+
+# Define the main method for running the TV program
+    
+# Use pyfiglet to print "TV Program" in ASCII art      
+
+# Create two TV instances
+
+# Turn on TV 1, set the channel and volume
+
+# Turn on TV 2, increase the channel twice and volume once
+
+# Print the current channel and volume for both TVs
+
+# Run the TestTV program
+
